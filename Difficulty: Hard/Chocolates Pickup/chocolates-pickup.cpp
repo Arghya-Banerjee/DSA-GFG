23e525bb6f653118588f7c8fb48fc00adca51dd9
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2025-03-01 16:12:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int solve(int n, int m, vector<vector<int>>& grid) {
+        // code here
+        int dp[n][m][m];
+        for(int j1 = 0; j1 < m; j1++){
+            for(int j2 = 0; j2 < m; j2++){
+                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j1 = 0; j1 < m; j1++){
+                for(int j2 = 0; j2 < m; j2++){
+                    int maxi = 0;
+                    for(int dx1 = -1; dx1 <= 1; dx1++){
+                        for(int dx2 = -1; dx2 <= 1; dx2++){
+                            int val = 0;
+                            if(j1 == j2) val = grid[i][j1];
+                            else val = grid[i][j1] + grid[i][j2];
+                            if(j1+dx1 >= 0 && j1+dx1 < m && j2+dx2 >= 0 && j2+dx2 < m){
+                                val += dp[i+1][j1+dx1][j2+dx2];
+                            }
+                            else val += -1e8;
+                            maxi = max(maxi, val);
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid;
+        for (int i = 0; i < n; i++) {
+            vector<int> temp;
+            for (int j = 0; j < m; j++) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            grid.push_back(temp);
+        }
+
+        Solution obj;
+        cout << obj.solve(n, m, grid) << "\n";
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
